0225187fcfda7ff760b621f0d45549562819bec9
Jerry Stevens <jerry@jerrystevens.nl>
2018-10-12 13:14:07 +0200

    fixed -e (exclude) switch

--- notes.sh
+++ notes.sh
@@ -17,29 +17,47 @@ NOTESFILE="$NOTESDIR/$FILENAME"
 if ! (($#)); then
 	# no arguments, print file
 	cat "$NOTESFILE"
-elif [[ "$1" == "-c" ]]; then
-	# clear file
-	rm "$NOTESFILE"
-elif [[ "$1" == "-e" ]]; then
-	# test if already exluded
-	grep -q '^.NOTES' "$NOTESDIR/.git/info/exclude"
-	if [ $? -ne 0 ]; then
-		# git exclude file in local repo
-		echo .NOTES >> "$NOTESDIR/.git/info/exclude"
-		echo "note-file excluded from git"
-	else
-		echo "note-file already excluded or this is not a git repo"
-	fi
-elif [[ "$1" == "-h" ]]; then
-	# show notes info and help
-	echo "notes-file: $NOTESFILE"
-	echo "git-branch: $BRANCH"
-	echo "Usage: notes.sh [-c] [-h] [note]"
-	echo -e "\twith no arguments it will print all notes"
-	echo -e "-c\tclear, delete all notes in this file"
-	echo -e "-e\texclude notes-file from git"
-	echo -e "-h\thelp, this message"
-else
-	# add all arguments to file prefixed with the date.
-	printf "%s %s | %s\n" $(date +'%F') "$BRANCH" "$*" >> "$NOTESFILE"
+	exit 0
 fi
+
+while (( "$#" )); do
+	if [[ "$1" == "-c" ]]; then
+		# clear file
+		rm "$NOTESFILE"
+		shift
+	elif [[ "$1" == "-b" ]]; then
+		# branch or category name
+		BRANCH="$2"
+		shift 2
+	elif [[ "$1" == "-e" ]]; then
+		if [[ -e "$NOTESDIR/.git/info/exclude" ]]; then
+			# test if already exluded
+			grep -q '^.NOTES' "$NOTESDIR/.git/info/exclude"
+			if [ $? -ne 0 ]; then
+				# git exclude file in local repo
+				echo .NOTES >> "$NOTESDIR/.git/info/exclude"
+				echo "note-file excluded from git"
+			else
+				echo "note-file already excluded"
+			fi
+		else
+			echo "not a git repository"
+		fi
+		shift
+	elif [[ "$1" == "-h" ]]; then
+		# show notes info and help
+		echo "notes-file: $NOTESFILE"
+		echo "git-branch: $BRANCH"
+		echo "Usage: $0 [-c] [-h] [-e]  [-b NAME] [NOTE...]"
+		echo -e "\twith no arguments it will print all notes"
+		echo -e "-c\tclear, delete all notes in this file"
+		echo -e "-e\texclude notes-file from git if in a repository"
+		echo -e "-b\tUse 'NAME' instead of branch name as category"
+		echo -e "-h\thelp, this message"
+		shift
+	else
+		# add all arguments to file prefixed with the date.
+		printf "%s %s | %s\n" $(date +'%F') "$BRANCH" "$*" >> "$NOTESFILE"
+		shift $#
+	fi
+done
